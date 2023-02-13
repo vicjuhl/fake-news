172d4edc7bd3ca9c2f861ad1388b643f64f7d498
vicjuhl <victorkjellerupjuhl@me.com>
2023-02-13 22:15:59 +0100

    TrainingData class

--- src/data_importer.py
+++ src/data_importer.py
@@ -1,15 +1,39 @@
 import pathlib as pl
 import pandas as pd
 
-def get_training_data(file_path: pl.Path) -> pd.DataFrame:
-    """Import training data from csv file."""
-    df = pd.read_csv(file_path)
-    df = df.drop(df.columns[0], axis=1)
-    df["type"] = pd.Categorical(df.type)
-    return df
+class TrainingData:
+    def __init__(self, file_path: pl.Path) -> None:
+        """Instantiate TrainingData class object."""
+        self.df = self.get_training_data(file_path)
+        self.rinse_training_data()
+        self.add_labels()
 
-def rinse_training_data(df: pd.DataFrame) -> pd.DataFrame:
-    """Rinse training data."""
-    df = df[df['type'].notna()]
-    return df
+    def get_training_data(self, file_path: pl.Path) -> pd.DataFrame:
+        """Import and typecast training data from csv file."""
+        df = pd.read_csv(file_path)
+        df = df.drop(df.columns[0], axis=1)
+        df["type"] = pd.Categorical(df.type)
+        return df
 
+    def rinse_training_data(self) -> None:
+        """Rinse training data."""
+        df = self.df[self.df['type'].notna()]
+        self.df = df
+
+    def add_labels(self) -> None:
+        """Add custom labels based on 'type' column."""
+        labels = {
+            "unreliable": "unknown",
+            "fake": "fake",
+            "clickbait": "unknown",
+            "conspiracy": "fake",
+            "reliable": "real",
+            "bias": "unknown",
+            "hate": "unknown",
+            "junksci": "fake",
+            "political": "unknown",
+            "unknown": "unknown"
+        }
+        def lookup_labels(data) -> pd.Categorical:
+            return labels[data["type"]]
+        self.df["labels"] = self.df.apply(lookup_labels, axis=1)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 import pathlib as pl
 import pandas as pd
-from data_importer import get_training_data, rinse_training_data
+from data_importer import TrainingData as TD
 
 if __name__ == "__main__":
     # parent -> src/, parent.parent -> fake-news/
     file_path = pl.Path(__file__).parent.parent.resolve() / "data_files/news_sample.csv"
-    trn_data = rinse_training_data(get_training_data(file_path))
-    print(trn_data["type"])
-    labels = trn_data.type.unique()
+    trn = TD(file_path)
+    print(trn.df["type"])
+    labels = trn.df.type.unique()
     for label in labels:
         print(label)
