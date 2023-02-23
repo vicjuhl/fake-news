5b82027dc9830b458dc961bed2691f19831f5706
vicjuhl <victorkjellerupjuhl@me.com>
2023-02-23 15:30:22 +0100

    count_sort and preprocess

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import pathlib as pl
 import pandas as pd
 from data_importer import TrainingData as TD
+from preprocessing.noise_removal import preprocess
+from cleantext import clean
 
 if __name__ == "__main__":
     # parent -> src/, parent.parent -> fake-news/
     file_path = pl.Path(__file__).parent.parent.resolve() / "data_files/news_sample.csv"
     trn = TD(file_path)
-    print(trn.df["type"])
-    labels = trn.df.type.unique()
-    for label in labels:
-        print(label)
+    tkns = preprocess(trn.df)
+    print(tkns)
+    
--- src/preprocessing/noise_removal.py
+++ src/preprocessing/noise_removal.py
@@ -1,7 +1,9 @@
+import numpy as np
 import pandas as pd
 from cleantext import clean
 
 def clean_text(df: pd.DataFrame) -> pd.DataFrame:
+    """Clean text for various anomalies."""
     df.content = df.content.apply(lambda x: clean(x,
         lower=True,
         normalize_whitespace=True,
@@ -12,9 +14,27 @@ def clean_text(df: pd.DataFrame) -> pd.DataFrame:
     ))
     return df
 
-def tokenize(df: pd.DataFrame) -> tuple[list[str], set[str]]:
+def tokenize(df: pd.DataFrame) -> list[str]:
+    """Generate list of tokens from dataframe."""
     content_tkns: list[list[str]] = [c.split(" ") for c in df["content"]]
     content_tkns_combined = []
     for lst in content_tkns:
         content_tkns_combined += lst
-    return (content_tkns_combined, set(content_tkns_combined))
+    return content_tkns_combined
+
+def count_sort(tkns: list[str]) -> pd.DataFrame:
+    """Creat dataframe with tokens as rows and frequency as values."""
+    counts = dict()
+    for tkn in tkns:
+        counts[tkn] = counts.get(tkn, 0) + 1
+    df = pd.DataFrame.from_dict(counts, orient="index", columns=["freq"])
+    df.sort_values(by=["freq"], ascending=False, inplace=True)
+    return df
+
+def preprocess(df: pd.DataFrame) -> pd.DataFrame:
+    """Run the preprocessing pipeline."""
+    df = clean_text(df)
+    tkns = tokenize(df)
+    counts = count_sort(tkns)
+    no_head = counts[50:]
+    return no_head
