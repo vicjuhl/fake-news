93c02564f6b9d032eb0391c94d718c1c4b1401a6
HP2706 <92665875+HP2706@users.noreply.github.com>
2023-03-10 14:35:45 +0100

    jj

--- src/run_s_model.py
+++ src/run_s_model.py
@@ -2,11 +2,11 @@
 from preprocessing import noise_removal as nr
 
 from model_specific_processing import simple_model as sm
-
+import os
 import pandas as pd
 
-
-df = pd.read_csv('data_files/processed_csv/reduced_corpus_csv.csv')
+csv_path = os.path.join(os.path.dirname(__file__), '..', 'data_files', 'processed_csv', 'reduced_corpus_csv.csv')
+df = pd.read_csv(csv_path)
 
 def run_model_on_df(df: pd.DataFrame):
     preprocessed_df = nr.preprocess(df)
